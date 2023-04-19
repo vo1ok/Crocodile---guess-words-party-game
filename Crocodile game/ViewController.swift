36bbd1966938b64dc450f66587b117ad9c617333
Olesia Kuklina <o.a.kuklina@yandex.ru>
2023-04-18 20:19:45 -0700

    Add mainVC

---
+++ Crocodile game/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Crocodile game
+//
+//  Created by Lesya Kotty on 18.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
